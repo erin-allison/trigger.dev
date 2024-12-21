86197c92454d292204972d1a0a9a698a417a80bb
Erin Allison <eallison@andrettikarting.com>
2024-12-21 14:13:24 -0500

    Add ability to customize memory limit for Docker app
    Signed-off-by: Erin Allison <eallison@andrettikarting.com>

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -12,7 +12,11 @@ pnpm --filter @trigger.dev/database db:migrate:deploy
 cp internal-packages/database/prisma/schema.prisma apps/webapp/prisma/
 cp node_modules/@prisma/engines/*.node apps/webapp/prisma/
 
+if [ -z "$MAX_OLD_SPACE_SIZE" ]; then
+  MAX_OLD_SPACE_SIZE=8192
+fi
+
 cd /triggerdotdev/apps/webapp
 # exec dumb-init pnpm run start:local
-NODE_PATH='/triggerdotdev/node_modules/.pnpm/node_modules' exec dumb-init node --max-old-space-size=8192 ./build/server.js
+NODE_PATH='/triggerdotdev/node_modules/.pnpm/node_modules' exec dumb-init node --max-old-space-size "$MAX_OLD_SPACE_SIZE" ./build/server.js
 
